63c1fe08a07c4be2723c83c03b9ce271c5d99883
atartavazd <artavazd@sunrisedvp.com>
2021-12-02 19:00:03 +0400

    hajox gnacinq tun

--- script.js
+++ script.js
@@ -4,10 +4,16 @@ document.body.onload = addElement;
 
 function addElement () {
   const newDiv = document.createElement("div");
+  let div2 =  document.createElement("div");
+  div2.className = 'demo2'
   const newContent = document.createTextNode("Hi there and greetings!");
+  const newContent2 = document.createTextNode(Date());
   newDiv.appendChild(newContent);
-  const currentDiv = document.getElementById("div");
+  div2.appendChild(newContent2);
+  const currentDiv = document.getElementById("div1");
+  let currentDiv2 = document.getElementById("div2");
   document.body.insertBefore(newDiv, currentDiv);
+  document.body.insertBefore(div2, currentDiv2);
 }
 
 let arr =  [1,2,3, 'apple'];
@@ -24,4 +30,5 @@ for(let i= 0 ; i<6; i++){
 
 }
 
+ 
 
