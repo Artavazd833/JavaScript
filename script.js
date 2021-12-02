9b02ba4e3841c3c55017b2252c84987978830431
atartavazd <artavazd@sunrisedvp.com>
2021-12-02 17:44:34 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log('hello world');
+document.getElementById('demo').innerHTML='Hello World';
+document.body.onload = addElement;
+
+function addElement () {
+  // create a new div element
+  const newDiv = document.createElement("div");
+
+  // and give it some content
+  const newContent = document.createTextNode("Hi there and greetings!");
+
+  // add the text node to the newly created div
+  newDiv.appendChild(newContent);
+
+  // add the newly created element and its content into the DOM
+  const currentDiv = document.getElementsByTagName("div");
+  document.body.insertBefore(newDiv, currentDiv);
+}
