54a4912462233868b68257849b346df6e882e762
atartavazd <artavazd@sunrisedvp.com>
2021-12-07 16:40:24 +0400

    react app

---
+++ my-app/src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+
+      <h1>React</h1>
+      <h2>Your Company's way to hold more cash in hands!</h2>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
